2af390a68b727ab456830ae332baa174de16ca55
attischen <b07902113@ntu.edu.tw>
2021-06-29 21:49:58 +0800

    enemy

---
+++ src/Assets/My Asset/Script/Life/Behavior/Idle.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Behaviors
+{
+    public class Idle : ABehavior
+    {
+        override public void go()
+        {
+            
+        } 
+    }
+}
+
+
